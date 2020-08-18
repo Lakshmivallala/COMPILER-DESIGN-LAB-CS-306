7828bef7b88e549bff223cfe1e5dac41aded59c5
Lakshmivallala <lakshmivallala@gmail.com>
2020-08-18 12:18:06 +0530

    Problem 2 commit second

--- Week1-Lab-exercise/Problem 2/end_same_symbol.c
+++ Week1-Lab-exercise/Problem 2/end_same_symbol.c
@@ -1,25 +1,83 @@
 #include<stdio.h>
-#include<stdlib.h> //for built-in function exit
-#include <string.h>
+#include<stdlib.h>
 void main()
 {
-	char token,input[20];
-	int n;
+	int state=0;
+	int i=0;
+	char token,input[30];
 	printf("Enter the input string: \n");
 	scanf("%s",input);
-	n = strlen(input);
-	while((token=input[n-1])!='\0' && (token=input[n-2])!='\0') 
+	while((token=input[i++])!='\0')
 	{
-		if (input[n-1]==input[n-2])
+		switch(state)
 		{
-			printf("String has been accepted.");
-			exit(0);
-		}
 
-		else
-		{
-			printf("String has not been accepted.");
-			exit(0);
+			case 0:
+			if(token=='a')
+				state=1;
+			else if(token=='b')
+				state=3;
+			else
+			{
+				printf("Invalid token");
+				exit(0);
+			}
+			break;
+
+
+			case 1:
+			if(token=='a')
+				state=2;
+			else if(token=='b')
+				state=3;
+			else
+			{
+				printf("Invalid token");
+				exit(0);
+			}
+			break;
+
+
+			case 2:
+			if(token=='a')
+				state=2;
+			else if(token=='b')
+				state=3;
+			else
+			{
+				printf("Invalid token");
+				exit(0);
+			}
+			break;
+
+			case 3:
+			if(token=='a')
+				state=1;
+			else if(token=='b')
+				state=4;
+			else
+			{
+				printf("Invalid token");
+				exit(0);
+			}
+			break;
+
+
+			case 4:
+			if(token=='a')
+				state=1;
+			else if(token=='b')
+				state=4;
+			else
+			{
+				printf("Invalid token");
+				exit(0);
+			}
+			break;
 		}
 	}
+	if((state==2)||(state==4))
+		printf("String has been accepted.");
+	else
+		printf("The string has not been accetped.");
 }
